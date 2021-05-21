a06199ce092156b83eebfbe0adaa465adcc632a9
Vikki Okamura <vcokamura@gmail.com>
2021-05-20 22:04:24 -0700

    Set up React and Sass

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>
+          This is a flashcards app.
+        </h1>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
